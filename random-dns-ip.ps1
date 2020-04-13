836fc08447a3c36122d2815210823cc9aa14fb93
spatronn <safaphlvnlr@gmail.com>
2020-04-14 00:31:46 +0300

    Create random-dns-ip.ps1

---
+++ random-dns-ip.ps1
@@ -0,0 +1,13 @@
+for ($i=1; $i -le 0; $i++)
+
+{
+$chars = [char[]]"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+$DNS = (($chars[0..25]|Get-Random)+(($chars|Get-Random -Count 14) -join ""))
+
+function RandomIPv4 {
+return [IPAddress]::Parse([String] (Get-Random) )
+}
+$IP =  (RandomIPv4).IPAddressToString
+
+Add-DnsServerResourceRecordA -Name $DNS -ZoneName "contoso.com" -IPv4Address $IP
+}
